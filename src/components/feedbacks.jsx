1ce669b9dcd0460d31dd40c0123331a3a1c6ec04
Lucas Campos <lucasoliveiracampos81@gmail.com>
2023-12-14 15:43:23 -0300

    feat: started feedbacks component.

--- src/components/feedbacks.jsx
+++ src/components/feedbacks.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Feedbacks = () => {
-  return <div>#INDICAÇÕES</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-xl text-orange-600">#INDICAÇÕES</h1>
+    </div>
+  );
 };
 
 export default Feedbacks;
